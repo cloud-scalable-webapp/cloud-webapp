d421b226ba0c8e884ac2a3c7ef3756831d2b02b8
Lokesh Challagulla <challagulla.l@northeastern.edu>
2023-01-31 10:16:45 -0500

    user model

---
+++ src/user/user-model.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const attributes = {
+        first_name:{ type: DataTypes.STRING, allowNull: false },
+        last_name:{ type: DataTypes.STRING, allowNull: false },
+        username: { type: DataTypes.STRING, allowNull: false },
+        password: { type: DataTypes.STRING, allowNull: false },
+        account_created:{
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          account_updated:{
+            type: DataTypes.STRING,
+            allowNull: false
+          }
+    };
+
+    const options = {
+        defaultScope: {
+            attributes: { exclude: ['password'] }
+        },
+        scopes: {
+            withPassword: { attributes: {}, }
+        }
+    };
+    return sequelize.define('Users', attributes, options);
+}
