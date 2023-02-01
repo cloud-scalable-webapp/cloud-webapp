39a03aae45547e0b8e8ed0827efb1df37c292498
Lokesh Challagulla <challagulla.l@northeastern.edu>
2023-02-01 07:26:26 -0500

    unit test

---
+++ user.test.js
@@ -0,0 +1,25 @@
+const server = require('../../server');
+const request = require('supertest');
+var expect = require('chai').expect;
+
+const fn = async x => {
+    return new Promise(resolve => {
+        setTimeout(resolve, 3000, 2*x);
+    });
+};
+(async function () {
+    const z = await fn(6);
+    describe('User Service Tests', function() {
+        const data = {
+            first_name: "Test",
+            last_name: "User",
+            username: "test@example.com",
+            password: "pass123"
+        };
+        it('Health Check', function() {
+            request(server).get('/healthz')
+                    .expect(200);
+        });
+    })
+    run();
+})();
