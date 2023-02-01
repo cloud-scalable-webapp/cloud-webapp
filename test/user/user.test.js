fb26cb58792ef64e614ac33930a33e2865ac58a9
Lokesh Challagulla <challagulla.l@northeastern.edu>
2023-02-01 09:26:33 -0500

    update test

--- test/user/user.test.js
+++ test/user/user.test.js
@@ -13,12 +13,40 @@ const fn = async x => {
         const data = {
             first_name: "Test",
             last_name: "User",
-            username: "test@example.com",
-            password: "pass123"
+            username: "test@domain.com",
+            password: "TestPassword123"
         };
         it('Health Check', function() {
             request(server).get('/healthz')
                     .expect(200);
+        });
+
+        it('Create a user', async function() {
+            const res = await request(server)
+                    .post('/v1/user')
+                    .send(data)
+                    .expect(201);                    
+          });
+
+        it('Get a user by ID without credentials', async function() {
+            const res = await request(server)
+                    .get('/v1/user/1')
+                    .send(data)
+                    .expect(401);    
+            expect(res.body.message).equals('Missing Authorization Header');                   
+        });
+
+        it('Get a user by ID with valid credentials', async function() {
+            const res = await request(server)
+                    .get('/v1/user/1')
+                    .auth(data.username, data.password)
+                    .expect(200);    
+            expect(res.body.id).equals(1);      
+            expect(res.body.first_name).equals(data.first_name);                   
+            expect(res.body.last_name).equals(data.last_name);                   
+            expect(res.body.username).equals(data.username);                   
+            expect(res.body.account_created).to.not.equal(null);               
+            expect(res.body.account_updated).to.not.equal(null);                 
         });
     })
     run();
