50c41095c97a8d9c7e1d0e32652625ab82a3ffc3
Lokesh Challagulla <challagulla.l@northeastern.edu>
2023-02-06 09:54:39 -0500

    fixing erros and auth

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -4,20 +4,26 @@ function errorHandler(err, res, res, next) {
     console.log(err)
     switch (true) {
         case typeof err === 'string':
-            const is404 = err.toLowerCase().endsWith('not found');
+            const is404 = err.toLowerCase().includes('not found');
             const isUserAlreadyPresent = err.toLowerCase().includes('is already registered');
             const userForbidden = err.toLowerCase().includes('change username');
             const bad = err.toLowerCase().includes('bad');
             const email = err.toLowerCase().includes('enter a valid email');
-
+            const productDeleteNotFound = err.includes('Product is not present in the database');
+            const productDeleteNotAvailable = err.includes('You cannot delete this product');
+            const productUpdateNotAvailable = err.includes('You are forbidden to update this product');
+            const notValidQuantity = err.includes('Enter a valid quantity');
+            const skuExists = err.includes('enter a different SKU');
+            const noData = err.includes('required fields');
+            const forbidden = err.includes('Forbidden');
             let statusCode;
-            if(is404) {
+            if(is404 || productDeleteNotFound ) {
                 statusCode = 404;
-            } else if (isUserAlreadyPresent || bad || email) {
+            } else if (isUserAlreadyPresent || bad || email || notValidQuantity || skuExists || noData ) {
                 statusCode = 400;
-            }else if(userForbidden){
+            }else if(userForbidden | productDeleteNotAvailable | productUpdateNotAvailable | forbidden ){
                 statusCode = 403;
-            }
+            }      
             return res.status(statusCode).json({ message: err });
         case err.name === 'UnauthorizedError':
             return res.status(401).json({ message: 'Unauthorized' });
